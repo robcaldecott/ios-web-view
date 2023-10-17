5087766c40de150f019eb83e507fec9499bedf99
Rob Caldecott <robert.caldecott@gmail.com>
2023-10-17 09:22:09 +0100

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { WebView } from "react-native-webview";
+
+const Stack = createNativeStackNavigator();
+
+function Home(props) {
+  return (
+    <SafeAreaView style={styles.screen}>
+      <Pressable
+        onPress={() => {
+          props.navigation.navigate("Web");
+        }}
+      >
+        <Text>Open web screen</Text>
+      </Pressable>
+    </SafeAreaView>
+  );
+}
+
+function Web(props) {
+  return (
+    <WebView
+      source={{ uri: "https://www.hibanapay.com/privacy/privacy.txt" }}
+    />
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Web" component={Web} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 32,
   },
 });
